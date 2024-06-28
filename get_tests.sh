2f5dfc71577f216bc2f808477a626bfc4e4e5826
funnsam <funnsam8@gmail.com>
2024-06-28 19:06:55 +0800

    rm tests

---
+++ get_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -e blargg-tests ]; then
+    git clone git@github.com:retrio/gb-test-roms.git blargg-tests --depth 1
+fi
+
+if [ ! -e mooneye-tests ]; then
+    MOONEYE_V=mts-20240127-1204-74ae166
+    wget "https://gekkio.fi/files/mooneye-test-suite/$MOONEYE_V/$MOONEYE_V.tar.xz" && {
+        tar -xvf $MOONEYE_V.tar.xz
+        mv $MOONEYE_V mooneye-tests
+        rm $MOONEYE_V.tar.xz
+    }
+fi
+
+if [ ! -e dmg-acid2.gb ]; then
+    wget "https://github.com/mattcurrie/dmg-acid2/releases/download/v1.0/dmg-acid2.gb"
+fi
